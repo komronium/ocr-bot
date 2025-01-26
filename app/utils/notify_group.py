f4c3264b290344fbfa2a56f76799b912c1636ae4
komronium <komronium@gmail.com>
2025-01-26 21:25:18 +0500

    make text beauty

--- app/utils/notify_group.py
+++ app/utils/notify_group.py
@@ -4,10 +4,10 @@ from config import settings
 async def notify_group_about_new_user(bot: Bot, user):
     try:
         message = (
-            f"*A new user has joined!\n*"
-            f"*ID:* {user.id}\n"
-            f"*Username:* @{user.username if user.username else 'N/A'}\n"
-            f"*Full Name:* {user.first_name} {user.last_name if user.last_name else ''}"
+            f"*👤 New Member!\n*"
+            f"*🆔 ID:* {user.id}\n"
+            f"*📛 Username:* {user.first_name} {user.last_name if user.last_name else ''}"
+            f"*✏️ Name:* @{user.username if user.username else 'N/A'}\n"
         )
         await bot.send_message(settings.GROUP_ID, message)
     except Exception as e:
