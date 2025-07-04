85f4d9a4a9d3fe4c3ee77a037bc2d28210c2261d
komronium <komronium@gmail.com>
2025-07-04 20:05:03 +0500

    Multi-language support

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -1,4 +1,6 @@
 import io
+import numpy as np
+import cv2
 import platform
 import pytesseract
 from PIL import Image
@@ -9,8 +11,17 @@ if platform.system() == "Windows":
 
 async def extract_text_from_image(file_content):
     try:
-        image = Image.open(io.BytesIO(file_content.read()))
-        text = pytesseract.image_to_string(image, lang='eng+rus+ara+aze+uzb+ukr+fas')
+        pil_image = Image.open(io.BytesIO(file_content.read()))
+        image = np.array(pil_image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        thresh = cv2.adaptiveThreshold(
+            gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY, 11, 2
+        )
+
+        text = pytesseract.image_to_string(thresh, lang='eng+rus+ara+aze+uzb+ukr+fas')
         return text.strip()
     except Exception as e:
         print(f"Error in OCR processing: {e}")
