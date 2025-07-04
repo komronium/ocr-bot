ee2b86dff9c02a90b8441df5172ea256d3934727
komronium <komronium@gmail.com>
2025-07-04 19:48:12 +0500

    Multi-language support

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -10,7 +10,7 @@ if platform.system() == "Windows":
 async def extract_text_from_image(file_content):
     try:
         image = Image.open(io.BytesIO(file_content.read()))
-        text = pytesseract.image_to_string(image, lang='rus+eng')
+        text = pytesseract.image_to_string(image, lang='eng+rus+ara+aze+uzb+ukr+fas')
         return text.strip()
     except Exception as e:
         print(f"Error in OCR processing: {e}")
